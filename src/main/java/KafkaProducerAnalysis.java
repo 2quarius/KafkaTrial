1167f9dbc2f758c365c8c95a6b0e6ea64e9accb7
2quarius <863115889@qq.com>
2019-05-18 16:49:44 +0800

    v2.1.0

--- src/main/java/KafkaProducerAnalysis.java
+++ src/main/java/KafkaProducerAnalysis.java
@@ -1,7 +1,11 @@
 import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
+import java.util.concurrent.Future;
 
 public class KafkaProducerAnalysis {
     public static final String brokerList = "localhost:9092";
@@ -9,10 +13,10 @@ public class KafkaProducerAnalysis {
 
     public static Properties initConfig(){
         Properties prop = new Properties();
-        prop.put("bootstrap.servers",brokerList);
-        prop.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        prop.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        prop.put("client.id","producer.client.id.demo");
+        prop.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,brokerList);
+        prop.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        prop.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG,StringSerializer.class.getName());
+        prop.put(ProducerConfig.CLIENT_ID_CONFIG,"producer.client.id.demo");
         return prop;
     }
 
@@ -21,7 +25,9 @@ public class KafkaProducerAnalysis {
         KafkaProducer<String,String> producer = new KafkaProducer<String, String>(props);
         ProducerRecord<String,String> record = new ProducerRecord<String, String>(topic,"hello,kafka!");
         try{
-            producer.send(record);
+            Future<RecordMetadata> future = producer.send(record);
+            RecordMetadata metadata = future.get();
+            System.out.println(metadata.topic()+'-'+metadata.partition()+":"+metadata.offset());
         }catch (Exception e){
             e.printStackTrace();
         }
